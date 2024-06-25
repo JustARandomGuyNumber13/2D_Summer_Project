daa47ac779eb8ebf3b3ce322d5c7b010135f5a2f
Tri <trivu2021@gmail.com>
2024-06-25 12:37:21 -0500

    Update
    Thêm nhân vật Vegeta
    Thêm di chuyển cho Player
    Thêm animation cho Vegeta

---
+++ Assets/JustARandomGuy/Scripts/Player/P_Movement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class P_Movement : MonoBehaviour
+{
+    [SerializeField] private P_Stat stat;
+    [SerializeField] private Animator anim;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+
+    private void Move()
+    {
+        int inputX = (int)Input.GetAxisRaw("Horizontal");                                   // Get keyboard input
+        rb.velocity = new Vector3(inputX * stat.MoveSpeed, rb.velocity.y);        // Apply movement velocity
+        anim.SetInteger("inputX", inputX);                                                           // Animation
+
+        if (inputX < 0 && transform.localScale.x > 0)                                          // Flip/Rotate character
+            Helper_FlipPlayer();
+        else if (inputX > 0 && transform.localScale.x < 0)
+            Helper_FlipPlayer();
+
+    }
+    private void Helper_FlipPlayer()
+    {
+        Vector3 scale = transform.localScale;
+        transform.localScale = new Vector3(-scale.x, scale.y, scale.z);
+    }
+}
